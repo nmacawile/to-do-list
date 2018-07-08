900d9264f7c6b899bc8542aef4d2f9b7f4dacf4e
Mac <nmacawile@gmail.com>
2018-07-08 07:21:49 +0000

    Add methods for removing and adding tasks

--- src/project.js
+++ src/project.js
@@ -4,9 +4,21 @@ const projectFactory = name => {
   const tasks = []
   
   const id = index ++
+  
+  const contains = task => tasks.find(content => content.id === task.id)
 
   const add = task => {
-    tasks.push(task)
+    if (!contains(task)) {
+      task.projectId = id
+      tasks.push(task)
+    }
+  }
+  
+  const remove = task => {
+    const taskIndex = tasks.indexOf(task)
+    if (taskIndex !== -1) {
+      taskIndex.splice(taskIndex, 1)
+    }
   }
 
   return { id, name, tasks, add }
