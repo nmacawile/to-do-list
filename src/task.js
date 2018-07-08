669a8f8d82c033fcdb9eadb5c03ed427df9667eb
Mac <nmacawile@gmail.com>
2018-07-08 07:20:08 +0000

    Fix Task module

--- src/task.js
+++ src/task.js
@@ -1,5 +1,29 @@
-const Task = (name, desc, dueDate, priority) => {
-  return { name, desc, dueDate, priority }
+let index = 1
+
+const taskFactory = (name, dueDate, priority, projectId, complete = false) => {
+  const id = index ++
+
+  return { id, name, dueDate, priority, projectId, complete }
 }
+
+
+const Task = (() => {
+  const list = []
+
+  const build = name => taskFactory(name)
+
+  const create = (name, dueDate, priority, projectId, complete) => {
+    const task = taskFactory(name, dueDate, priority, projectId, complete)
+    list.push(task)
+    return task
+  }
+
+  return {
+    build, create,
+    get all() {
+     return list
+    } 
+  }
+})()
 
 export default Task
